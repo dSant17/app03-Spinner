012d9dd4dbb43b4ca3203cb8ff3daa5f443342ea
Dan Santana <2019030386@upsin.edu.mx>
2022-05-14 13:32:02 -0600

    Archivos de la práctica 3 - Spinner no personalizado

---
+++ app/src/main/java/com/example/app03/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.app03;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spnPaises;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spnPaises = (Spinner) findViewById(R.id.spnPaises);
+        ArrayAdapter<String> Adaptador = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.paises));
+        spnPaises.setAdapter(Adaptador);
+        spnPaises.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Seleccionó el país " + adapterView.getItemAtPosition(i).toString() + ".", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
